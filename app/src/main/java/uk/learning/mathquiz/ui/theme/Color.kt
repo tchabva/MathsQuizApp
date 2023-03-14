9b35ba0176c0c1ec76ddaddb5a9b3ea615d40c25
Tawanda Chabva <tchabva>
2023-03-14 22:35:38 +0000

    Updated Colours

--- app/src/main/java/uk/learning/mathquiz/ui/theme/Color.kt
+++ app/src/main/java/uk/learning/mathquiz/ui/theme/Color.kt
@@ -2,12 +2,19 @@ package uk.learning.mathquiz.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+/*
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
+ */
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val Green1 = Color(0x0,177,89)
+val GreenMain = Color(0xFF00b159)
+val BlueDivision = Color(0xFF00aedb)
+val RedMultiply = Color (0xFFd11141)
+val Orange = Color(0xFFf37735)
+val Yellow2 = Color(0xFFffc425)
