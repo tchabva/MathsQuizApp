93dd59b998c35a1bd72573a7111f17a936023755
Tawanda Chabva <tchabva>
2023-04-01 00:01:03 +0100

    Start of the Number Table Dialog

---
+++ app/src/main/java/uk/learning/mathquiz/ui/resultsHistoryScreen/NumberTableDialog.kt
@@ -0,0 +1,13 @@
+package uk.learning.mathquiz.ui.resultsHistoryScreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.Dialog
+
+@Composable
+fun NumbersDialog(){
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+    
+    Dialog(onDismissRequest = { /*TODO*/ }) {
+        
+    }
+}
