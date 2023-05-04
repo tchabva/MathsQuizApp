06a47c9ce992492720fb2b8db041a766ac5a2b6e
tchabva <tchabva@gmail.com>
2023-05-04 22:58:20 +0100

    Change the No Filter state name

--- app/src/main/java/uk/learning/mathquiz/ui/resultsHistoryScreen/HistoryScreenState.kt
+++ app/src/main/java/uk/learning/mathquiz/ui/resultsHistoryScreen/HistoryScreenState.kt
@@ -3,7 +3,7 @@ package uk.learning.mathquiz.ui.resultsHistoryScreen
 This creates the states on the HistoryScreen that will work with the filters in the toolbar
  */
 sealed class HistoryScreenState(){
-    class FiltersInactive(): HistoryScreenState()
+    class NoFilter(): HistoryScreenState()
 
     class FiltersActive(): HistoryScreenState()
 }
