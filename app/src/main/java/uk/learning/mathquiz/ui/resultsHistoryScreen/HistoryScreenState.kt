74a7462be85791621a992215fccfb9c31e97bba0
tchabva <tchabva@gmail.com>
2023-05-05 00:49:24 +0100

    Adjusting the HistoryScreenState parameters to work as intended

--- app/src/main/java/uk/learning/mathquiz/ui/resultsHistoryScreen/HistoryScreenState.kt
+++ app/src/main/java/uk/learning/mathquiz/ui/resultsHistoryScreen/HistoryScreenState.kt
@@ -2,8 +2,17 @@ package uk.learning.mathquiz.ui.resultsHistoryScreen
 /*
 This creates the states on the HistoryScreen that will work with the filters in the toolbar
  */
-sealed class HistoryScreenState(){
-    class NoFilter(): HistoryScreenState()
+sealed class HistoryScreenState(
+    val storedOperator: String?,
+    val storedNumber: String?
+){
+    class NoFilter(
+        storedOperator: String? = null,
+        storedNumber: String? = null
+    ): HistoryScreenState(storedOperator, storedNumber)
 
-    class FiltersActive(): HistoryScreenState()
+    class FiltersActive(
+        storedOperator: String,
+        storedNumber: String
+    ): HistoryScreenState(storedOperator, storedNumber)
 }
